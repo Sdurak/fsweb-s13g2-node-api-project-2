9ee800a576c9bfcc4a05007038d60ca83cadb38d
Sdurak <sdurak91@hotmail.com>
2023-06-13 18:55:23 +0300

    S13G2 Proje

--- api/server.js
+++ api/server.js
@@ -1,3 +1,15 @@
 // server için gerekli olanları burada ayarlayın
-
+const express = require("express");
+const server = express();
+server.use(express.json());
 // posts router'ını buraya require edin ve bağlayın
+// const postModel = require("./posts/posts-model");
+const postsRouter = require("./posts/posts-router");
+
+server.get("/", (req, res) => {
+  res.send("blabla2");
+});
+
+server.use("/api/posts", postsRouter);
+
+module.exports = server;
